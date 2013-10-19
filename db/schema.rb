d9617ae9f3d2292ed4681f0c27de95f88860372c
Rahul <techniciablog@gmail.com>
2013-10-19 19:43:50 +0530

    Before implementing comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019063820) do
+ActiveRecord::Schema.define(version: 20131019131304) do
+
 
   create_table "links", force: true do |t|
     t.integer  "user_id"
